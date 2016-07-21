a21351ef2e2ce9b00a9161a2958891e4b28f6d7f
Scott M. Barbour <smbarbour@gmail.com>
2016-07-20 23:20:15 -0500

    Fixed imports a little better

--- src/main/java/org/mcupdater/packbuilder/gui/MainForm.java
+++ src/main/java/org/mcupdater/packbuilder/gui/MainForm.java
@@ -3,6 +3,7 @@ package org.mcupdater.packbuilder.gui;
 import org.mcupdater.api.Version;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainForm {
 
@@ -20,10 +21,17 @@ public class MainForm {
 	private void initGui() {
 		frameMain = new JFrame();
 		frameMain.setIconImage(new ImageIcon(this.getClass().getResource("mcu-icon.png")).getImage());
-		frameMain.setTitle("MCUpdater " + Version.VERSION + Version.BUILD_LABEL);
+		frameMain.setTitle("MCU PackBuilder " + Version.VERSION + Version.BUILD_LABEL);
 		frameMain.setBounds(100, 100, 1175, 592);
 		frameMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		JPanel panelLeft = new JPanel();
+		panelLeft.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 1, Color.BLACK));
+		panelLeft.setLayout(new BorderLayout(0, 0));
+		{
+
+		}
+		frameMain.getContentPane().add(panelLeft, BorderLayout.WEST);
 	}
 
 	private void bindLogic() {
